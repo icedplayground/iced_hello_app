0c2c0a3fa7dc1669c3bedef6f9b0cc9e12779025
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-10-02 19:08:22 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,29 @@ use iced::widget::{center, text};
 use iced::{Element, Result, Task};
 use iced::application;
 
-#[allow(non_camel_case_types)]
-#[derive(Default)]
-struct MY_ICED_HELLO_WORLD_STRUCT;
 
-#[derive(Debug, Clone, Copy)]
-enum Message {}
+// ============================== //
+
 
 // fn main
 fn main() -> Result {
     application(MY_ICED_HELLO_WORLD_STRUCT::title, MY_ICED_HELLO_WORLD_STRUCT::update, MY_ICED_HELLO_WORLD_STRUCT::view)
         .run_with(|| (MY_ICED_HELLO_WORLD_STRUCT::default(), Task::none()))
 }
+
+
+// ============================== //
+// iced magic happens here
+
+// struct - important for state
+#[allow(non_camel_case_types)]
+#[derive(Default)]
+struct MY_ICED_HELLO_WORLD_STRUCT;
+
+// enum
+// The message defines any events or interactions that your program will care about.
+#[derive(Debug, Clone, Copy)]
+enum Message {}
 
 // impl
 impl MY_ICED_HELLO_WORLD_STRUCT {
